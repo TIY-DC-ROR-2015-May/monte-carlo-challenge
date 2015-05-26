3846dd51cde3fcd6ecf512aa7419e41e2714d50a
Polly Sileo <pollysileo@gmail.com>
2015-05-26 09:27:50 -0400

    Passing all tests

--- dice.rb
+++ dice.rb
@@ -1,5 +1,31 @@
 class Dice
+  attr_reader :sides
+  def initialize sides
+    @sides = sides
+  end
+
+  def roll 
+    rand(1..@sides)
+  end
+
+  def loaded?
+    return false
+  end
+  
 end
 
-class LoadedDice
+class LoadedDice <     Dice
+  def but_really_is_it_loaded?
+    return true
+  end
+
+  def roll
+    n = rand(1..3)
+    if n >1
+      return rand(3..@sides)
+    else
+      return rand(1..@sides)
+    end
+  end
+
 end
